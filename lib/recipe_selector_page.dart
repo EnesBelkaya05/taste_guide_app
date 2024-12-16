9bf17cc2d822caa3b131d1cf0f86f9aec9951a96
EnesBelkaya05 <eb.belkaya@gmail.com>
2024-12-16 13:03:34 +0300

    beggining

--- lib/recipe_selector_page.dart
+++ lib/recipe_selector_page.dart
@@ -12,8 +12,6 @@ class RecipeSelectorPage extends StatefulWidget {
 
 ApiService apiService = new ApiService();
 
-//Armin Büyük ADAMDIR.
-
 class _RecipeSelectorPageState extends State<RecipeSelectorPage> {
   final List<String> recipeTypes = ['Vegetarian', 'Vegan', 'Meat', 'Dessert'];
   final List<String> selectedIngredients = [];
